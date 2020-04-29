86b74b929626db1d42f76879b65b96ecdd360443
Michael Hogg <michael.hogg.uk@gmail.com>
2020-04-29 21:51:02 +0100

    Create src/constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,20 @@
+/**
+ * @license MIT
+ * @see github.com/michaelhogg/sdsim
+ */
+
+//----------------------------------------------------------------------//
+//                          Exported constants                          //
+//----------------------------------------------------------------------//
+
+/** @see photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Components.Depth.html */
+export const LAYER_DEPTH_PERSON:         number = 20;
+export const LAYER_DEPTH_PERSONPAIRLINE: number = 10;
+
+/**
+ * @see photonstorm.github.io/phaser3-docs/Phaser.GameObjects.GameObject.html#setData
+ * @see photonstorm.github.io/phaser3-docs/Phaser.GameObjects.GameObject.html#getData
+ */
+export const GO_DATA_KEYS: { [key: string]: string } = {
+  COLLISION_PRODUCTSHELF: 'COLLISION_PRODUCTSHELF'
+};
